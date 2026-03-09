d609f46b19f7c11a9de254aec51fbbcf0909ea7e
zayennn <zaayeennn@gmail.com>
2026-03-10 05:47:00 +0700

    update

--- 01. Struktur Dasar Program Assembly/1. Section Program/hello.asm
+++ 01. Struktur Dasar Program Assembly/1. Section Program/hello.asm
@@ -1,11 +1,37 @@
-@ section .data
-@ section .bss
-@ section .text
+@ * section .data
+@ * section .bss
+@ * section .text
 
-@ section .data → tempat data yang sudah punya nilai
-@ contoh :
+@ ? section .data → tempat data yang sudah punya nilai
+@ todo contoh :
 
 section .data 
     angka db 10
     huruf db 'a'
     pesan db "halo", 0
+
+@ * angka db 10 :
+@ angka = nama variable
+@ db    = define byte
+@ 10    = nilai yang di simpan ( value )
+@ * cpu nanti akan menyimpan angka 10 di memory
+
+@ * huruf db 'A' :
+@ akan menyimpan huruf A kedalam memory
+
+@ * pesan db "hallo", 0
+@ ini string, 0 di akhir disebut null terminator. biasanya dipakai buat mengakhiri sebuah string
+
+@ ? section .bss → tempat variable yang belum punya nilai
+@ todo contoh :
+
+section .bss
+    buffer resb 64
+
+@ * buffer resb 64 :
+@ cpu cuma menyediakan 64 byte memory kosong
+
+@ ? section .text → tempat code program di jalankan ( semua instruksi cpu ada di sini )
+@ todo contoh :
+
+section .
