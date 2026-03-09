002fb1bfdc394a91322ef8fc5f17ac69c9b01882
zayennn <zaayeennn@gmail.com>
2026-03-10 05:38:11 +0700

    update

--- 01. Struktur Dasar Program Assembly/1. Section Program/hello.asm
+++ 01. Struktur Dasar Program Assembly/1. Section Program/hello.asm
@@ -1,5 +1,11 @@
-section .data
-section .bss
-section .text
+@ section .data
+@ section .bss
+@ section .text
 
 @ section .data → tempat data yang sudah punya nilai
+@ contoh :
+
+section .data 
+    angka db 10
+    huruf db 'a'
+    pesan db "halo", 0
